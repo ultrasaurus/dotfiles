ba242f49f875556cec36ec03c5094cdef7c7d973
Sarah Allen <sarah@ultrasaurus.com>
2020-01-12 00:13:45 -0800

    liquidprompt is slow

--- liquidprompt.sh
+++ liquidprompt.sh
@@ -2,4 +2,4 @@
 
 [[ -d $DIR/liquidprompt ]] || git clone https://github.com/nojhan/liquidprompt.git $DIR/liquidprompt
 
-[[ $- = *i* ]] && source $DIR/liquidprompt/liquidprompt
+# [[ $- = *i* ]] && source $DIR/liquidprompt/liquidprompt
